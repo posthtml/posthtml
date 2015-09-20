bc0d854de67cb4e2faa7891f474e2aa74b5cc57e
Ivan Voischev <voischev.ivan@ya.ru>
2015-09-20 13:15:51 +0300

    feat(parser): toHtml tests

--- test/parser.js
+++ test/parser.js
@@ -5,19 +5,82 @@ import path from 'path';
 import fs from 'fs';
 
 const applecom = fs.readFileSync(path.resolve(__dirname, 'templates/applecom.html'), 'utf8').toString();
-const html = fs.readFileSync(path.resolve(__dirname, 'templates/parser.html'), 'utf8').toString();
-const tree = require('./templates/parser.js');
+const parserHtml = fs.readFileSync(path.resolve(__dirname, 'templates/parser.html'), 'utf8').toString();
+const parserTree = require('./templates/parser.js');
 
 describe('Parser', () => {
 
-    it('toTree', done => {
-        expect(tree).to.eql(toTree(html));
-        done();
+    describe('toTree', () => {
+        it('html to tree', done => {
+            expect(parserTree).to.eql(toTree(parserHtml));
+            done();
+        });
     });
 
-    it('toHtml', done => {
-        expect(html).to.eql(toHtml(tree));
-        done();
+    describe('toHtml', () => {
+        it('tree to html', done => {
+            expect(parserHtml).to.eql(toHtml(parserTree));
+            done();
+        });
+
+        it('skip string', done => {
+            expect('skip string').to.eql(toHtml('skip string'));
+            done();
+        });
+
+        it('default single tags', done => {
+            const TAGS = ['area', 'base', 'br', 'col', 'command', 'embed', 'hr', 'img', 'input', 'keygen', 'link', 'menuitem', 'meta', 'param', 'source', 'track', 'wbr'];
+            let tree = TAGS.map(tag => ({ tag: tag }));
+            let html = TAGS.map(tag => `<${tag}>`).join('');
+            expect(html).to.eql(toHtml(tree));
+            done();
+        });
+
+        describe('options', () => {
+            describe('singleTags', () => {
+                it('set tags', done => {
+                    let tree = [{ tag: 'rect' }, { tag: 'custom' }];
+                    let html = '<rect><custom>';
+
+                    expect(html).to.eql(toHtml(tree, { singleTags: ['rect', 'custom']}));
+                    done();
+                });
+
+                it('safe attrs', done => {
+                    let tree = [{ tag: 'rect', attrs: { id: 'foo' } }];
+                    let html = '<rect id="foo">';
+
+                    expect(html).to.eql(toHtml(tree, { singleTags: ['rect' ]}));
+                    done();
+                });
+            });
+
+            describe('closingSingleTag', () => {
+                it('default', done => {
+                    let tree = [{ tag: 'img' }];
+                    let html = '<img>';
+
+                    expect(html).to.eql(toHtml(tree, { closingSingleTag: 'default'}));
+                    done();
+                });
+
+                it('slash', done => {
+                    let tree = [{ tag: 'img' }];
+                    let html = '<img/>';
+
+                    expect(html).to.eql(toHtml(tree, { closingSingleTag: 'slash'}));
+                    done();
+                });
+
+                it('tag', done => {
+                    let tree = [{ tag: 'img' }];
+                    let html = '<img></img>';
+
+                    expect(html).to.eql(toHtml(tree, { closingSingleTag: 'tag'}));
+                    done();
+                });
+            });
+        });
     });
 
     it('toTree => toHtml', done => {
