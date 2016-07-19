746cb5e453ab4fa5397b0428caefd252be646045
Jeff Escalante <hello@jenius.me>
2016-07-19 16:43:43 -0400

    tests

--- test/index.js
+++ test/index.js
@@ -3,24 +3,66 @@ const path = require('path')
 const fs = require('fs')
 const posthtml = require('..')
 const customElements = require('posthtml-custom-elements')
+const exp = require('posthtml-exp')
+const sugarml = require('sugarml')
+const codegen = require('posthtml-code-gen')
 const fixtures = path.join(__dirname, 'fixtures')
 
 test('basic', (t) => {
-  const html = fs.readFileSync(path.join(fixtures, 'basic.html'))
-
-  posthtml({ plugins: [customElements()] })
-    .process(html)
-    .then((res) => {
-      t.truthy(res.output === '<div class="custom">hi</div>\n')
-      t.truthy(res.options)
-      t.truthy(res.runtime)
-    })
+  process('basic.html', { plugins: [customElements()] }).then((res) => {
+    t.truthy(res.output === '<div class="custom">hi</div>\n')
+    t.truthy(res.options)
+    t.truthy(res.runtime)
+  })
 })
 
-test.todo('custom parser')
-test.todo('custom generator')
-test.todo('parser options')
-test.todo('generator options')
-test.todo('options override to process method')
-test.todo('single plugin')
-test.todo('multi plugin')
+test('custom parser', (t) => {
+  process('sugarml.html', { parser: sugarml }).then((res) => {
+    t.truthy(res.output.trim() === '<p>hello world!</p>')
+  })
+})
+
+test('custom generator', (t) => {
+  process('basic.html', { generator: codegen }).then((res) => {
+    t.truthy(res.output() === '<custom>hi</custom>')
+  })
+})
+
+test('parser options', (t) => {
+  process('upcase.html', {
+    parserOptions: { lowerCaseTags: true }
+  }).then((res) => {
+    t.truthy(res.output === '<div>hi</div>')
+  })
+})
+
+// right now generator options not working correctly
+test.skip('generator options', (t) => {
+  process('selfclosing.html', {
+    generator: codegen,
+    generatorOptions: { selfClosing: 'slash' }
+  }).then((res) => {
+    t.truthy(res.output() === '<br />')
+  })
+})
+
+test('options override to process method', (t) => {
+  process('basic.html', { plugins: [customElements()] }, {
+    plugins: [(x) => x]
+  }).then((res) => {
+    t.truthy(res.output === '<custom>hi</custom>')
+  })
+})
+
+test('multi plugin', (t) => {
+  process('expression.html', {
+    plugins: [customElements(), exp({ locals: { foo: 'bar' } })]
+  }).then((res) => {
+    t.truthy(res.output.trim() === '<div class="custom">bar</div>')
+  })
+})
+
+function process (file, config, config2) {
+  const html = fs.readFileSync(path.join(fixtures, file), 'utf8')
+  return posthtml(config).process(html, config2)
+}
