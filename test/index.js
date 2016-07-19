d4836b9346de67f69f5b77515276264f1deb2501
Jeff Escalante <hello@jenius.me>
2016-07-19 15:36:55 -0400

    api extend, first test

--- test/index.js
+++ test/index.js
@@ -1,9 +1,22 @@
 const test = require('ava')
 const path = require('path')
+const fs = require('fs')
 const posthtml = require('..')
+const customElements = require('posthtml-custom-elements')
 const fixtures = path.join(__dirname, 'fixtures')
 
-test.todo('basic')
+test('basic', (t) => {
+  const html = fs.readFileSync(path.join(fixtures, 'basic.html'))
+
+  posthtml({ plugins: [customElements()] })
+    .process(html)
+    .then((res) => {
+      t.truthy(res.output === '<div class="custom">hi</div>\n')
+      t.truthy(res.options)
+      t.truthy(res.runtime)
+    })
+})
+
 test.todo('custom parser')
 test.todo('custom generator')
 test.todo('parser options')
