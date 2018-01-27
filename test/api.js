90e3565190424b48d5b407062f5d51f1aa33ccb0
Ivan Demidov <scrum@list.ru>
2018-01-27 17:40:59 +0300

    revert(api): return export api methods

--- test/api.js
+++ test/api.js
@@ -4,11 +4,8 @@ var describe = require('mocha').describe
 
 var posthtml = require('../lib')
 
-var Api = require('../lib/api')
-var api = new Api()
-
-var walk = api.walk
-var match = api.match
+var walk = require('../lib/api').walk
+var match = require('../lib/api').match
 
 function test (nodes, reference, fn, options, done) {
   expect(posthtml([].concat(fn))
