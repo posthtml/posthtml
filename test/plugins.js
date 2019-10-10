8c71b1aebee1b78919258034955cd2a65ef92550
Ivan Demidov <Scrum@list.ru>
2019-10-10 16:14:03 +0300

    test(plugins): change methods name

--- test/plugins.js
+++ test/plugins.js
@@ -238,14 +238,14 @@ describe('Plugins', function () {
     it('tree should have core methods', function () {
       posthtml()
         .use(function (tree) {
-          tree.should.have.property('toString')
-          tree.should.have.property('fromString')
+          tree.should.have.property('render')
+          tree.should.have.property('parser')
           tree.toString.should.be.a('function')
         })
         .process('<div></div>')
     })
 
-    it('core methods fromString', function (done) {
+    it('core methods parser', function (done) {
       var html = '<import>'
       var ref = '<div>import</div>'
 
@@ -253,7 +253,7 @@ describe('Plugins', function () {
         .use(function (tree) {
           tree.match({ tag: 'import' }, function (node) {
             node.tag = false
-            node.content = tree.fromString('<div>import</div>')
+            node.content = tree.parser('<div>import</div>')
             return node
           })
           return tree
@@ -269,15 +269,15 @@ describe('Plugins', function () {
         })
     })
 
-    it('core methods toString', function (done) {
+    it('core methods render', function (done) {
       var html = '\n<div>1</div>\n\t<div>2</div>\n'
       var ref = '<div>1</div><div>2</div>'
 
       posthtml()
         .use(function (tree) {
           var outherTree = ['\n', { tag: 'div', content: ['1'] }, '\n\t', { tag: 'div', content: ['2'] }, '\n']
-          var htmlWitchoutSpaceless = tree.toString(outherTree).replace(/[\n|\t]/g, '')
-          return tree.fromString(htmlWitchoutSpaceless)
+          var htmlWitchoutSpaceless = tree.render(outherTree).replace(/[\n|\t]/g, '')
+          return tree.parser(htmlWitchoutSpaceless)
         })
         .process(html)
         .then(function (result) {
