a9d0467b2e531e7b91cde64c0f9dae764bd5cc97
Ivan Demidov <Scrum@list.ru>
2021-07-28 10:56:30 +0300

    perf: after dep update

--- test/parser.js
+++ test/parser.js
@@ -4,8 +4,8 @@ const path = require('path')
 const { it, describe } = require('mocha')
 const { expect } = require('chai')
 
-const { default: parser } = require('posthtml-parser')
-const { default: render } = require('posthtml-render')
+const { parser } = require('posthtml-parser')
+const { render } = require('posthtml-render')
 
 const html = readFileSync(
   path.resolve(__dirname, 'templates/parser.html'), 'utf8'
