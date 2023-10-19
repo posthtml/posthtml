404a97a16de6a829d571096c781d17d2df7d4b40
Ivan Demidov <scrum@list.ru>
2023-10-19 17:43:29 +0300

    build: update husky

--- vitest.config.js
+++ vitest.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    include: "test/*.js",
+    include: ["test/*.js"],
     coverage: {
       provider: 'v8',
       reporter: ['lcov', 'text'],
