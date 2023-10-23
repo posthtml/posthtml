b28eddf1bd84ab0fb1134d0501ab939ee21cf3af
Ivan Demidov <scrum@list.ru>
2023-10-23 14:42:39 +0300

    test: not need watch flug when cover

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    watch: false,
     coverage: {
       provider: 'v8',
       reporter: ['lcov', 'text'],
